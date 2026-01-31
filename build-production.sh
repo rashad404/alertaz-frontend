4168fb4634d47484e87190cd667dbdfcfe6b5e7b
Rashad Mirza <rashad.mirza.404@gmail.com>
2026-01-31 14:41:15 -0600

    build vers fix

--- build-production.sh
+++ build-production.sh
@@ -20,8 +20,8 @@ echo ""
 # Update build version and widget cache buster
 echo "🔄 Updating build version..."
 BUILD_VERSION=$(date +%s)
-sed -i '' "s/NEXT_PUBLIC_BUILD_VERSION=.*/NEXT_PUBLIC_BUILD_VERSION=${BUILD_VERSION}/" .env.production
-sed -i '' "s/NEXT_PUBLIC_SATIS_WIDGET_URL=.*/NEXT_PUBLIC_SATIS_WIDGET_URL=https:\/\/api.satis.az\/widget.js?v=${BUILD_VERSION}/" .env.production
+sed -i "s/NEXT_PUBLIC_BUILD_VERSION=.*/NEXT_PUBLIC_BUILD_VERSION=${BUILD_VERSION}/" .env.production
+sed -i "s|NEXT_PUBLIC_SATIS_WIDGET_URL=.*|NEXT_PUBLIC_SATIS_WIDGET_URL=https://api.satis.az/widget.js?v=${BUILD_VERSION}|" .env.production
 echo "✓ Build version set to: ${BUILD_VERSION}"
 echo ""
 
